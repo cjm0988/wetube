907ae23c61d2a90118a0f72b0e6b03b67d9797b7
cjm0988 <45810279+cjm0988@users.noreply.github.com>
2019-02-10 01:34:23 +0900

    1-14 home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "SICA",
+            email: "cjm0988@naver.com"
+        }
+    },
+    {
+        id: 23232323,
+        title: "Video nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "SICA",
+            email: "cjm0988@naver.com"
+        }
+    },
+    {
+        id: 34343434,
+        title: "Video super",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "SICA",
+            email: "cjm0988@naver.com"
+        }
+    },
+    {
+        id: 45454545,
+        title: "Video hyper",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "SICA",
+            email: "cjm0988@naver.com"
+        }
+    }
+];
