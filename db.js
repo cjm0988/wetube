4800494a3c1ee98b0903f7dff318bc4f855f9028
cjm0988 <45810279+cjm0988@users.noreply.github.com>
2019-02-10 16:02:33 +0900

    2-1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,50 +1,19 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "SICA",
-            email: "cjm0988@naver.com"
-        }
-    },
-    {
-        id: 23232323,
-        title: "Video nice",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "SICA",
-            email: "cjm0988@naver.com"
-        }
-    },
-    {
-        id: 34343434,
-        title: "Video super",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "SICA",
-            email: "cjm0988@naver.com"
-        }
-    },
-    {
-        id: 45454545,
-        title: "Video hyper",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "SICA",
-            email: "cjm0988@naver.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = error => console.log(`Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
