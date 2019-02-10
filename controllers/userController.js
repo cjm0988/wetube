ff3cd11790d4b4f69ba2a95b955ac758412fe083
cjm0988 <45810279+cjm0988@users.noreply.github.com>
2019-02-10 13:34:00 +0900

    1-16 login and user profile controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,12 +12,20 @@ export const postJoin = (req, res) => {
         res.status(400);
         res.render("join", { pageTitle: "Join"});
     } else {
+        // To Do: Register User
+        // To Do: Log user in
         res.redirect(routes.home);
     }
 };
 
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const getLogin = (req, res) => 
+    res.render("login", {pageTitle: "Log In"});
+    
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
+export const logout = (req, res) => res.render("logout", {pageTitle: "Log Out"});
+export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
+export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
+export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
